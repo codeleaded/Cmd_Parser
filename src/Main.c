806635b6ea8b496bdadfcda1aa99155f5e595c3a
C-Reaper <gnu.c.reaper@gmail.com>
2025-07-30 00:46:32 +0200

    init commit

---
+++ src/Main.c
@@ -0,0 +1,14 @@
+#include "C:/Wichtig/System/Static/Library/Parser.h"
+
+int main(int argc, const char *argv[]){
+    
+    Parser p = Parser_New();
+
+    Parser_Parse(&p,"int a = \"hello world\n\" + 10 + 0b1010101");
+
+    Parser_Print(&p);
+
+    Parser_Free(&p);
+
+    return 0;
+}

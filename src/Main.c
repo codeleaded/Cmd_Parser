40494d28d6953c09ee20a298c55a34413eb44e24
C-Reaper <gnu.c.reaper@gmail.com>
2025-08-03 21:28:00 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,13 +1,10 @@
-#include "C:/Wichtig/System/Static/Library/Parser.h"
+#include "/home/codeleaded/System/Static/Library/Parser.h"
 
 int main(int argc, const char *argv[]){
     
     Parser p = Parser_New();
-
     Parser_Parse(&p,"int a = \"hello world\n\" + 10 + 0b1010101");
-
     Parser_Print(&p);
-
     Parser_Free(&p);
 
     return 0;

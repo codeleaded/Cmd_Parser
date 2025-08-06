058953ef5a29f7156cb0c9afb40ef7cd43e05d6f
codeleaded <codeleaded@gmail.com>
2025-08-06 14:08:22 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -4,6 +4,11 @@ int main(int argc, const char *argv[]){
     
     Parser p = Parser_New();
     Parser_Parse(&p,"int a = \"hello world\n\" + 10 + 0b1010101");
+    
+    //Parser_Classify
+    Parser_Parse_Space(&p,"float a = 10.0 + 0.1");
+    //Parser_PushToken
+    
     Parser_Print(&p);
     Parser_Free(&p);
 
